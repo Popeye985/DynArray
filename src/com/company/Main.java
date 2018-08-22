8e146974f6b442f5e0636989a1d44be84708bc2e
Popeye985 <ceulemans_koen@hotmail.com>
2018-08-22 15:30:26 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner invoer = new Scanner(System.in);
+        DynArray getallen = new DynArray();
+        int getal;
+
+
+
+
+        do {
+            System.out.println("Geef een getal");
+            getal = Integer.parseInt(invoer.nextLine());
+            if (getal >= 0){
+                getallen.add(getal);
+            }
+
+
+
+        }while (getal >= 0);
+
+        //Arrays.sort(getallen, 0, aantal);
+        for (int i = 0; i < getallen.size() ; i++) {
+            System.out.println(getallen.get(i));
+        }
+    }
+    }
+
